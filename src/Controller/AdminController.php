9b106a2507109b863f3233ff9bd170c9a5d97116
Ismail ANOUAR <ismael.anouar@gmail.com>
2022-09-09 09:59:23 +0000

    Style page dashboard v1

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,13 +5,24 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\IngredientRepository;
+use App\Repository\PizzaRepository;
 
 class AdminController extends AbstractController
 {
     #[Route('admin/dashboard', name: 'admin_app_dashboard')]
-    public function dashboard(): Response
+    public function dashboard(IngredientRepository $ingredientRepository, PizzaRepository $pizzaRepository): Response
     {
-        return $this->render('admin/dashboard.html.twig', []);
+        $ingredients = $ingredientRepository->findAll();
+        $pizzas = $pizzaRepository->findAll();
+
+        $total_pizzas = count($pizzas);
+        $total_ingredients = count($ingredients);
+
+        return $this->render('admin/dashboard.html.twig', [
+            'total_ingredients' => $total_ingredients,
+            'total_pizzas'      => $total_pizzas,
+        ]);
     }
 
     #[Route('admin/', name: 'admin_app_gestion')]
