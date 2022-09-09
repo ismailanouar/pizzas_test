926b684cdeb663f7454c69dd24a5fa2abdca926b
Ismail ANOUAR <ismael.anouar@gmail.com>
2022-09-09 12:53:32 +0000

    Style page Accueil v1

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,18 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PizzaRepository;
 
 class MainController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function home(): Response
+    public function home(PizzaRepository $pizzaRepository): Response
     {
         $user = $this->getUser();
+        $pizzas = $pizzaRepository->findAll();
+        $total_pizzas = count($pizzas);
+
         if(in_array("ROLE_ADMIN", $user->getRoles())){
             return $this->redirectToRoute("admin_app_gestion");
             exit;
         }
-        return $this->render('home/index.html.twig', []);
+        
+        return $this->render('home/index.html.twig', [
+            'pizzas' => $pizzas,
+            'total_pizzas' => $total_pizzas
+        ]);
     }
 
     #[Route('/', name: 'index')]
